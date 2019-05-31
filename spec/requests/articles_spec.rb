b8d19b00ab8510554b5b6c64125279186dc3efe8
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-05-30 22:10:47 -0400

    scaffold out ArticlesController

---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe ArticlesController do
+  describe "GET /articles" do
+    # test listing articles
+  end
+
+  describe "POST /articles" do
+    # test creating articles
+  end
+
+  describe "GET /articles/:id" do
+    # test getting article content
+  end
+
+  describe "PATCH /articles/:id" do
+    # test updating an article
+  end
+
+  describe "DELETE /articles/:id" do
+    # test deleting an article
+  end
+end
